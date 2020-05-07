fab0bf22fb125be244cc1260c95c5608f29d7c0a
coderphr <qq1730684350@163.com>
2020-05-07 16:19:39 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <!-- 顶部栏 -->
+     <mt-header :title="this.$route.name">
+      <router-link to="/" slot="left">
+        <mt-button icon="back">返回</mt-button>
+      </router-link>
+      <mt-button icon="more" slot="right"></mt-button>
+    </mt-header>
+
+    <!-- 底部栏 -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+}
+</style>
